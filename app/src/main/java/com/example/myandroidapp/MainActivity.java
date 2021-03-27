f8c4125681d075e7a4848a6bc779257c5e1c69c5
k-chan-l <kings0625@naver.com>
2021-03-27 12:39:53 +0900

    app view
    the first view of application

--- app/src/main/java/com/example/myandroidapp/MainActivity.java
+++ app/src/main/java/com/example/myandroidapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.myandroidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 데이터 원본 준비
+        String[] items = {"일", "월", "화", "수", "목", "금", "토"};
+        String[] items2 = {"", "", "1", "2", "3", "4", "5","6","7"};
+
+        //어댑터 준비 (배열 객체 이용, simple_list_item_1 리소스 사용
+        ArrayAdapter<String> adapt
+                = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                items);
+        ArrayAdapter<String> adapt2
+                = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                items2);
+
+        // id를 바탕으로 화면 레이아웃에 정의된 GridView 객체 로딩
+        GridView gridview = (GridView) findViewById(R.id.gridview1);
+        GridView gridview2 = (GridView) findViewById(R.id.gridview2);
+        // 어댑터를 GridView 객체에 연결
+        gridview.setAdapter(adapt);
+        gridview2.setAdapter(adapt2);
     }
 }
